68faa9a60397355834ce0fb9392084e4f81dc6f3
Adam Orłowski <orlowski.adam.99@gmail.com>
2023-03-26 14:37:10 +0200

    init

---
+++ src/main/java/com/kantakap/auctionfilehandler/AuctionFileHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.kantakap.auctionfilehandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionFileHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionFileHandlerApplication.class, args);
+    }
+
+}
