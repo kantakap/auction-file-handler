2a33ad98025ed5b8875f7aeb92f182de230ac037
Adam Orłowski <orlowski.adam.99@gmail.com>
2023-04-01 15:00:05 +0200

    disable CORS

--- src/main/java/com/kantakap/auctionfilehandler/controller/FileController.java
+++ src/main/java/com/kantakap/auctionfilehandler/controller/FileController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
 
+@CrossOrigin("*")
 @RestController
 @RequiredArgsConstructor
 public class FileController {
